633ce24654839821172f31680854337f5a2aac0b
ndlganeshu <ndlganeswararao@gmail.com>
2022-07-10 23:17:59 +0530

    4 files modified, 13 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HelloComponent } from './hello.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
